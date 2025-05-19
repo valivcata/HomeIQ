ba34063cd88e8f380591515f565d4f1f3ed48f4d
Vali <catalin.valentin.info@gmail.com>
2025-05-20 01:51:03 +0300

    init

---
+++ Mobile/homeiq-mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace homeiq_mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
